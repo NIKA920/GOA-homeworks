d85cb54e8c3d70af81694235cb172eff20d62b25
NIKA920 <nikazukhbaia19@gmail.com>
2024-07-16 23:34:41 +0400

    second commit

---
+++ day 03/classwork/cw.py
@@ -0,0 +1,15 @@
+library = "book"
+name = "gabrieli"
+car = "BMW"
+#number = 20
+
+#print(library + name + car)
+print(name + " " + car)
+
+
+#print(library)
+#print(name)
+#print(car)
+
+
+#ცვლადი veriable
---
+++ day 05/classwork/cw.py
@@ -0,0 +1,28 @@
+#writing - კოდის წერა
+#running - გაშვება/კოდის გაშვება
+#debugging - კოდის გასწორება/კოდშ არსებული შეცდომების გასწორება
+
+#print("hello")
+
+
+#parentheses - ფრჩხილებო
+#quotation marks -ბრჭყალები
+#define - განსაზღვრა/შექმნა
+#variable - ცვლადი
+#calue - განსაზღვრა
+#string - მონაცემთა ტიპირ რომელიც აღნიშნავს ბრჭყალებში მოქცეულ ტექსტს მაგალითად: "hello world"
+#ingert - მონაცემთა ტიპი რომელიც აღნიშნავს წილად რიცხვს მაგალითად: 1 2 52 14 31 
+#float - მონაცემთა ტიპი რომელიც აღნიშნავს წილად რიცხვს მაგალითად: 5.1 6.4 7.7
+#bug - შეცდომა/პრობლემა
+ 
+car = "BMW"
+car2 = "mercedes"
+print ("i love", car)
+print("i love", car2)
+
+print ("i love", car3)
+print("i love", car4)
+car3 = "audi"
+car4 = "ford"
+
+
